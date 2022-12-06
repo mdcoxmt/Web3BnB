986b8998e4a876009f404d8b71564949a70bb10a
Duncan Cox <mdcoxmt@gmail.com>
2022-12-06 14:21:58 -0700

    Added unit tests
    added web3bnbstay.js, also added a constructor inside Web3BnBStay.Sol

--- test/web3bnbstay.js
+++ test/web3bnbstay.js
@@ -1,11 +1,33 @@
-const { expect } = require('chai');
-const { ethers } = require("hardhat")
+const {ethers} = require("hardhat")
+const {expect} = require("chai")
 
-const { task } = require("hardhat/config");
+describe("Mint Web3BnBStay Contract", function() {
+    let Web3BnBStay, web3bnbstay;
+    let owner;
 
+    const TOKEN_URI = "DUMMY_TOKEN_URI"
 
-task("deploy", "Deploys the Web3BnBStay.sol contract").setAction(async function (taskArguments, hre) {
-    const nftContractFactory = await hre.ethers.getContractFactory("Web3BnB", getAccount());
-    const web3bnb = await nftContractFactory.deploy();
-    console.log(`Contract deployed to address: ${web3bnb.address}`);
-});
+    beforeEach(async function() {
+            Web3BnBStayFactory = await ethers.getContractFactory("Web3BnBStay");
+            [owner, hodler1] = await ethers.getSigners();
+            web3bnbstay = await Web3BnBStayFactory.deploy();
+    })
+
+    describe("Deployment", function() {
+        it("Should have correct owner", async function(){
+            expect(await web3bnbstay.owner()).to.equal(owner.address);
+        })
+
+        it("Should have correct balance after minting", async function() {
+            const initialBalance = await web3bnbstay.balanceOf(hodler1.address)
+            expect(initialBalance.toString()).to.equal("0");
+            
+            await web3bnbstay._safemint(holder1.address, tokenID);
+
+            const finalBalance = await web3bnbstay.balanceOf(hodler1.address)
+            expect(finalBalance.toString()).to.equal("1");
+        })
+    })
+   
+
+})
