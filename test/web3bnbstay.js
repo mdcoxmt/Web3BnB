77ef606e194caa31cfab05012978e2c84862bbae
Duncan Cox <mdcoxmt@gmail.com>
2022-12-06 20:47:00 -0700

    Modified mintStay function
    Added require to mintStay, wrote unit tests

--- test/web3bnbstay.js
+++ test/web3bnbstay.js
@@ -1,15 +1,17 @@
 const {ethers} = require("hardhat")
 const {expect} = require("chai")
+const provider = ethers.getDefaultProvider()
 
 describe("Mint Web3BnBStay Contract", function() {
     let Web3BnBStay, web3bnbstay;
     let owner;
 
     const TOKEN_URI = "DUMMY_TOKEN_URI"
+    const PROPERTY_URI = "DUMMY_PROPERTY_URI"
 
     beforeEach(async function() {
             Web3BnBStayFactory = await ethers.getContractFactory("Web3BnBStay");
-            [owner, hodler1] = await ethers.getSigners();
+            [owner, hodler1, hodler2] = await ethers.getSigners();
             web3bnbstay = await Web3BnBStayFactory.deploy();
     })
 
@@ -18,15 +20,20 @@ describe("Mint Web3BnBStay Contract", function() {
             expect(await web3bnbstay.owner()).to.equal(owner.address);
         })
 
-        it("Should have correct balance after minting", async function() {
-            const initialBalance = await web3bnbstay.balanceOf(hodler1.address)
-            expect(initialBalance.toString()).to.equal("0");
+        it("Caller should have correct STAY balance after minting", async function() {
+            const initialStayBalance = await web3bnbstay.balanceOf(hodler1.address)
+            expect(initialStayBalance.toString()).to.equal("0");
             
-            await web3bnbstay.mintStay(hodler1.address, TOKEN_URI);
+            await web3bnbstay.connect(hodler1).mintStay(TOKEN_URI, 2, 5, {value:"10"});
 
             const finalBalance = await web3bnbstay.balanceOf(hodler1.address)
             expect(finalBalance.toString()).to.equal("1");
         })
+
+        it("Should reject mintStay if caller's msg.value too low", async function(){
+            await expect(web3bnbstay.connect(hodler2).mintStay(TOKEN_URI, 100, 100, {value: "9999"})).to.be.revertedWith("You don't have enough funds to book this date")
+        })
+
     })
    
 
